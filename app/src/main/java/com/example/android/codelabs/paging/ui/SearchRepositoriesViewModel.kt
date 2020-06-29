a2da3449a5499a874bb1da5c7c666b4defb7b0c2
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-06-29 01:04:02 -0300

    12. Adding list separators

--- app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
+++ app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
@@ -19,9 +19,11 @@ package com.example.android.codelabs.paging.ui
 import androidx.lifecycle.*
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import androidx.paging.insertSeparators
 import com.example.android.codelabs.paging.data.GithubRepository
 import com.example.android.codelabs.paging.model.Repo
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 /**
  * ViewModel for the [SearchRepositoriesActivity] screen.
@@ -31,21 +33,55 @@ class SearchRepositoriesViewModel(private val repository: GithubRepository) : Vi
 
     private var currentQueryValue: String? = null
 
-    private var currentSearchResult: Flow<PagingData<Repo>>? = null
+    private var currentSearchResult: Flow<PagingData<UiModel>>? = null
 
     /**
      * Search a repository based on a query string.
      */
-    fun searchRepo(queryString: String) : Flow<PagingData<Repo>> {
+    fun searchRepo(queryString: String) : Flow<PagingData<UiModel>> {
         val lastResult = currentSearchResult
-        if(queryString == currentQueryValue && lastResult != null){
+        if (queryString == currentQueryValue && lastResult != null) {
             return lastResult
         }
 
         currentQueryValue = queryString
-        val newResult: Flow<PagingData<Repo>> = repository.getSearchResultStream(queryString)
+        val newResult: Flow<PagingData<UiModel>> = repository.getSearchResultStream(queryString)
+                .map { pagingData -> pagingData.map { UiModel.RepoItem(it) } }
+                .map {
+                    it.insertSeparators<UiModel.RepoItem, UiModel> { before, after ->
+                        if (after == null) {
+                            // we're at the end of the list
+                            return@insertSeparators null
+                        }
+
+                        if (before == null) {
+                            //we are at the beginning of the list
+                            return@insertSeparators UiModel.SeparatorItem("${after.roundedStarCount}0.000+ stars")
+                        }
+
+                        // check between 2 items
+                        if (before.roundedStarCount > after.roundedStarCount) {
+                            if (after.roundedStarCount >= 1) {
+                                UiModel.SeparatorItem("${after.roundedStarCount}0.000+ starts")
+                            } else {
+                                UiModel.SeparatorItem("< 10.000+ starts")
+                            }
+                        } else {
+                            // no separator
+                            null
+                        }
+                    }
+                }
                 .cachedIn(viewModelScope)
         currentSearchResult = newResult
         return newResult
     }
+
+    sealed class UiModel{
+        data class RepoItem(val repo: Repo) : UiModel()
+        data class SeparatorItem(val description: String) : UiModel()
+    }
+
+    private val UiModel.RepoItem.roundedStarCount: Int
+        get() = this.repo.stars / 10_000
 }
