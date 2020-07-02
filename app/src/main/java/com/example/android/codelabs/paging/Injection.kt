43aac19e301b3b495a34a5927e89fc8f1d3a3e17
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-07-02 00:46:22 -0300

    19. Update the paging Flow creation

--- app/src/main/java/com/example/android/codelabs/paging/Injection.kt
+++ app/src/main/java/com/example/android/codelabs/paging/Injection.kt
@@ -20,7 +20,9 @@ import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.codelabs.paging.api.GithubService
 import com.example.android.codelabs.paging.data.GithubRepository
+import com.example.android.codelabs.paging.db.RepoDatabase
 import com.example.android.codelabs.paging.ui.ViewModelFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 /**
  * Class that handles object creation.
@@ -33,15 +35,16 @@ object Injection {
      * Creates an instance of [GithubRepository] based on the [GithubService] and a
      * [GithubLocalCache]
      */
-    private fun provideGithubRepository(): GithubRepository {
-        return GithubRepository(GithubService.create())
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun provideGithubRepository(context: Context): GithubRepository {
+        return GithubRepository(GithubService.create(), RepoDatabase.getInstance(context))
     }
 
     /**
      * Provides the [ViewModelProvider.Factory] that is then used to get a reference to
      * [ViewModel] objects.
      */
-    fun provideViewModelFactory(): ViewModelProvider.Factory {
-        return ViewModelFactory(provideGithubRepository())
+    fun provideViewModelFactory(context: Context): ViewModelProvider.Factory {
+        return ViewModelFactory(provideGithubRepository(context))
     }
 }
--- app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -21,6 +21,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.db.RepoDatabase
 import com.example.android.codelabs.paging.model.Repo
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
@@ -29,7 +30,10 @@ import kotlinx.coroutines.flow.Flow
  * Repository class that works with local and remote data sources.
  */
 @ExperimentalCoroutinesApi
-class GithubRepository(private val service: GithubService) {
+class GithubRepository(
+        private val service: GithubService,
+        private val database: RepoDatabase
+) {
 
     /**
      * Search repositories whose names match the query, exposed as a stream of data that will emit
@@ -38,9 +42,20 @@ class GithubRepository(private val service: GithubService) {
      fun getSearchResultStream(query: String): Flow<PagingData<Repo>> {
         Log.d("GithubRepository", "New query: $query")
 
+        // appending '%' so we can allow other characters to be before and after the query string
+        val dbQuery = "%${query.replace(' ', '%')}%"
+        val pagingSourceFactory =  { database.reposDao().reposByName(dbQuery)}
+
+        //Old Implementation using the GithubPagingSource
+//        return Pager(
+//                config = PagingConfig(pageSize = NETWORK_PAGE_SIZE, maxSize = NETWORK_PAGE_SIZE * 5),
+//                pagingSourceFactory = {GithubPagingSource(service, query)}
+//        ).flow
+
         return Pager(
                 config = PagingConfig(pageSize = NETWORK_PAGE_SIZE, maxSize = NETWORK_PAGE_SIZE * 5),
-                pagingSourceFactory = {GithubPagingSource(service, query)}
+                remoteMediator = GithubRemoteMediator(query, service, database),
+                pagingSourceFactory = pagingSourceFactory
         ).flow
     }
 
